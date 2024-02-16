684f3dc3c408d246d61b7b995c0e4967fe0ea7cf
Kamande14918 <155572884+Kamande14918@users.noreply.github.com>
2024-02-16 22:28:11 +0300

    Update simple-calculator.c

--- simple-calculator.c
+++ simple-calculator.c
@@ -1,17 +1,17 @@
-// Kamande Kamau
-// ENE212-0169/2021
-
-// Simple C Calculator
+//Kamande Kennedy Kamau
+//Reg: ENE212-0169/2021
+//Simple C Calculator
 #include <stdio.h>
-
-int main(){
-    double num1,num2,sum,diff;
-    printf("Please enter two numbers\n");
-    scanf("%lf,%lf",&num1,&num2);
-    sum=num1 + num2;
+//Simple addition and subtraction calculator
+int main()
+{  
+    double num1,num2,sum,diff;        
+    printf("Please enter two numbers:\n");
+    scanf("%lf %lf", &num1, &num2);
+    sum=num1+num2;
     diff=num1-num2;
-    printf("Sum of the two numbers is:\n",sum);
-    printf("Difference of the two numbers is:\n",diff);
-    printf("Summation and Difference of the two numbers are:%f\t%f\t",sum,diff);
-  return 0;
+    printf("Summation  of the two numbers is :%f\n",sum);
+    printf("Difference between the two numbers is: %f\n",diff);
+    printf("Summation and Difference of the two numbers are:%f\t %f\t", sum, diff);
+    return 0;
 }
